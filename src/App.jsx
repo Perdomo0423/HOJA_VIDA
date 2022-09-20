f84a8b087a1ee02ccc9dbf69c14798120a044867
Perdomo0423 <juan_perdomoca@fet.edu.co>
2022-09-20 16:50:37 -0500

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <>
       <Routes>
-          <Route exact path="/" element={<Principal/>} />
+          <Route exact path="/HOJA_VIDA" element={<Principal/>} />
           <Route exact path="/HojaVida" element={<HojaPrincipal />} />
       </Routes>
       </>
